4d34fcf47bf34fc72b175e022f769f481811f0df
Abdul Muhaimin Toha <abdulmuhaimintoha.business@gmail.com>
2024-06-01 13:53:42 +0600

    converting router to array route

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,36 +1,44 @@
 import MainLayout from "@/layouts/MainLayout";
 import RegistrationPage from "@/pages/RegistrationPage/RegistrationPage";
 import SignInPage from "@/pages/SignInPage/SignInPage";
-import {
-  Route,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import LoggedInRoute from "./LoggedInRoute";
 import HomePage from "@/pages/HomePage/HomePage";
 import ErrorPage from "@/pages/ErrorPage/ErrorPage";
 
-export const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<MainLayout />}>
-      <Route index element={<HomePage />} />
-      <Route
-        path="sign-in"
-        element={
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "sign-in",
+        element: (
           <LoggedInRoute>
             <SignInPage />
           </LoggedInRoute>
-        }
-      />
-      <Route
-        path="registration"
-        element={
+        ),
+      },
+      {
+        path: "registration",
+        element: (
           <LoggedInRoute>
             <RegistrationPage />
           </LoggedInRoute>
-        }
-      />
-      <Route path="*" element={<ErrorPage />} />
-    </Route>,
-  ),
-);
+        ),
+      },
+      {
+        path: "*",
+        element: <ErrorPage />,
+      },
+    ],
+  },
+  {
+    path: "dashboard",
+    element: <h1>dashboard</h1>,
+  },
+]);
